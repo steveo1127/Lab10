ad4a6edf4c7e77f46e13c3d4a49f206e7011f154
Steven F <fredriks@umich.edu>
2024-04-11 15:51:59 -0400

    Changed Positive and Negative Test Cases

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -15,17 +15,17 @@ public class testSelectionSort {
 			}
 			public void testPositive(){
 			int[] arr = new int[5];
-			arr[0] = 8;
+			arr[0] = 4;
 			arr[1] = 9;
-			arr[2] = 7;
+			arr[2] = 13;
 			arr[3] = 10;
 			arr[4] = 2;
 			int[] Sortedarr = new int[5];
 			Sortedarr[0] = 2;
-			Sortedarr[1] = 7;
-			Sortedarr[2] = 8;
-			Sortedarr[3] = 9;
-			Sortedarr[4] = 10;
+			Sortedarr[1] = 4;
+			Sortedarr[2] = 9;
+			Sortedarr[3] = 10;
+			Sortedarr[4] = 13;
 			/** add tests to check for this unit test **/
 			SelectionSort positiveSort = new SelectionSort();
 			int[] output = positiveSort.basicSelectionSort(arr);
@@ -34,17 +34,17 @@ public class testSelectionSort {
 			public void testNegative(){
 				/** Test data contains negative values only **/
 				int[] arr = new int[5];
-				arr[0] = -8;
+				arr[0] = -15;
 				arr[1] = -9;
 				arr[2] = -7;
-				arr[3] = -10;
+				arr[3] = -1;
 				arr[4] = -2;
 				int[] Sortedarr = new int[5];
-				Sortedarr[0] = -10;
+				Sortedarr[0] = -15;
 				Sortedarr[1] = -9;
-				Sortedarr[2] = -8;
-				Sortedarr[3] = -7;
-				Sortedarr[4] = -2;
+				Sortedarr[2] = -7;
+				Sortedarr[3] = -2;
+				Sortedarr[4] = -1;
 				/** add tests to check for this unit test **/
 				SelectionSort negativeSort = new SelectionSort();
 				int[] output = negativeSort.basicSelectionSort(arr);
