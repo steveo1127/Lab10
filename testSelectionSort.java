2c1280f737ea6c0dd8f1d46efd0172c77efaa49c
Steven F <fredriks@umich.edu>
2024-04-11 15:56:40 -0400

    Modified dulpicates and mixed

--- testSelectionSort.java
+++ testSelectionSort.java
@@ -53,17 +53,17 @@ public class testSelectionSort {
 			public void testMixed(){
 				/** Test data contains with both positive, negative and zeros **/
 				int[] arr = new int[5];
-				arr[0] = -8;
-				arr[1] = -9;
-				arr[2] = -0;
-				arr[3] = 7;
+				arr[0] = -3;
+				arr[1] = -10;
+				arr[2] = 0;
+				arr[3] = 3;
 				arr[4] = 1;
 				int[] Sortedarr = new int[5];
-				Sortedarr[0] = -9;
-				Sortedarr[1] = -8;
+				Sortedarr[0] = -10;
+				Sortedarr[1] = -3;
 				Sortedarr[2] = 0;
 				Sortedarr[3] = 1;
-				Sortedarr[4] = 7;
+				Sortedarr[4] = 3;
 				/** add tests to check for this unit test **/
 				SelectionSort mixedSort = new SelectionSort();
 				int[] output = mixedSort.basicSelectionSort(arr);
@@ -72,17 +72,17 @@ public class testSelectionSort {
 			public void testDuplicates(){
 				/** Test data contains duplicates **/
 				int[] arr = new int[5];
-				arr[0] = 7;
-				arr[1] = 7;
+				arr[0] = 9;
+				arr[1] = 9;
 				arr[2] = 0;
-				arr[3] = -8;
-				arr[4] = -8;
+				arr[3] = -5;
+				arr[4] = -5;
 				int[] Sortedarr = new int[5];
-				Sortedarr[0] = -8;
-				Sortedarr[1] = -8;
+				Sortedarr[0] = -5;
+				Sortedarr[1] = -5;
 				Sortedarr[2] = 0;
-				Sortedarr[3] = 7;
-				Sortedarr[4] = 7;
+				Sortedarr[3] = 9;
+				Sortedarr[4] = 9;
 				/** add tests to check for this unit test **/
 				SelectionSort duplicateSort = new SelectionSort();
 				int[] output = duplicateSort.basicSelectionSort(arr);
